59326de909af0749a8c39211bbfffca08efaaa07
BioBootloader <thebiobootloader@gmail.com>
2023-07-27 15:45:10 -0700

    have to check python version in __main__

--- mentat/app.py
+++ mentat/app.py
@@ -1,9 +1,3 @@
-import sys
-
-# check must be located before importing code that uses 3.10 features
-if sys.version_info < (3, 10):
-    exit("Error: Python version 3.10 or higher is required.")
-
 import argparse
 import logging
 import os
