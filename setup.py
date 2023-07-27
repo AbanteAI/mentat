90d3eb242a98f4775c4893c6baf0b1129d35e096
BioBootloader <thebiobootloader@gmail.com>
2023-07-27 06:52:44 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_requirements(file):
 
 setup(
     name="mentat-ai",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=read_requirements("requirements.txt"),
     package_data={
