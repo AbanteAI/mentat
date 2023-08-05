0c0756ebe1e0d7d9846046cd6b85b37934cf257f
Paul Swingle <swingle8paul@gmail.com>
2023-08-05 16:40:57 -0600

    remove toolbar and instead use autocompleter

--- mentat/mentat_prompt_session.py
+++ mentat/mentat_prompt_session.py
@@ -2,10 +2,14 @@ import os
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.application.current import get_app
-from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory, Suggestion
+from prompt_toolkit.buffer import Buffer
+from prompt_toolkit.completion.word_completer import WordCompleter
+from prompt_toolkit.document import Document
 from prompt_toolkit.filters import Condition
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.key_processor import KeyPressEvent
 from prompt_toolkit.styles import Style
 
 from .config_manager import ConfigManager, mentat_dir_path
@@ -17,29 +21,43 @@ class FilteredFileHistory(FileHistory):
         super().__init__(filename)
 
     def append_string(self, string):
-        if string.strip().lower() not in self.excluded_phrases:
+        if (
+            string.strip().lower() not in self.excluded_phrases
+            # If the user mistypes a command, we don't want it to appear later
+            and string.strip()
+            and string.strip()[0] != "/"
+        ):
             super().append_string(string)
+
+
+class FilteredHistorySuggestions(AutoSuggestFromHistory):
+    def __init__(self):
+        super().__init__()
+
+    def get_suggestion(self, buffer: Buffer, document: Document) -> Suggestion | None:
+        # We want the auto completer to handle commands
+        if buffer.text[0] == "/":
+            return None
+        else:
+            return super().get_suggestion(buffer, document)
 
 
 class MentatPromptSession(PromptSession):
     def __init__(self, config: ConfigManager):
-        self.file_history = FilteredFileHistory(
-            os.path.join(mentat_dir_path, "history")
-        )
-        self.auto_suggest = AutoSuggestFromHistory()
-        self.style = Style(config.input_style())
         self._setup_bindings()
         super().__init__(
             message=[("class:prompt", ">>> ")],
-            history=self.file_history,
-            auto_suggest=self.auto_suggest,
-            style=self.style,
+            completer=WordCompleter(
+                words=["command words here"],
+                ignore_case=True,
+                sentence=True,
+            ),
+            history=FilteredFileHistory(os.path.join(mentat_dir_path, "history")),
+            auto_suggest=FilteredHistorySuggestions(),
+            style=Style(config.input_style()),
             multiline=True,
             prompt_continuation=self.prompt_continuation,
             key_bindings=self.bindings,
-            # Toolbar automatically gets the class bottom-toolbar, and the text gets the class bottom-toolbar.text
-            # Also, fg and bg are automatically reversed for the bottom toolbar (adding noreverse will undo this)
-            bottom_toolbar="",
         )
 
     def prompt_continuation(self, width, line_number, is_soft_wrap):
@@ -52,29 +70,27 @@ class MentatPromptSession(PromptSession):
 
         @self.bindings.add("s-down")
         @self.bindings.add("c-j")
-        def _(event):
+        def _(event: KeyPressEvent):
             event.current_buffer.insert_text("\n")
 
         @self.bindings.add("enter")
-        def _(event):
+        def _(event: KeyPressEvent):
             event.current_buffer.validate_and_handle()
 
         @Condition
-        def suggestion_available() -> bool:
+        def complete_suggestion() -> bool:
             app = get_app()
             return (
                 app.current_buffer.suggestion is not None
                 and len(app.current_buffer.suggestion.text) > 0
                 and app.current_buffer.document.is_cursor_at_the_end
+                and app.current_buffer.text
+                and app.current_buffer.text[0] != "/"
             )
 
         # c-i is the code for tab
-        @self.bindings.add("c-i", filter=suggestion_available)
-        def _(event):
+        @self.bindings.add("c-i", filter=complete_suggestion)
+        def _(event: KeyPressEvent):
             suggestion = event.current_buffer.suggestion
             if suggestion:
                 event.current_buffer.insert_text(suggestion.text)
-
-        @self.bindings.add("escape")
-        def _(event):
-            pass
