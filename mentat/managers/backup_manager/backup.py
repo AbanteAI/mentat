cf9ae9ae658d4c0980aff76c37d8ff566699ba20
simugrad <dtuname@gmail.com>
2023-08-09 16:07:57 +0300

    recursive backup

--- mentat/managers/backup_manager/backup.py
+++ mentat/managers/backup_manager/backup.py
@@ -27,17 +27,20 @@ class CodeBackupManager:
         for file_path in code_file_manager.get_all_file_paths():
             if self.backup_dir:
                 relative_path = os.path.relpath(file_path)
-                relative_path = relative_path.replace(os.path.sep, "_")
-                backup_file_path = os.path.join(
-                    self.backup_dir, relative_path + ".backup"
-                )
+                if os.path.sep in relative_path:
+                    parent_folder, file_name = os.path.split(relative_path)
+                    backup_folder = os.path.join(self.backup_dir, parent_folder)
+                    os.makedirs(backup_folder, exist_ok=True)
+                    backup_file_path = os.path.join(backup_folder, file_name + ".backup")
+                else:
+                    backup_file_path = os.path.join(self.backup_dir, os.path.basename(file_path) + ".backup")
             else:
                 backup_file_path = file_path + ".backup"
 
             if not os.path.exists(backup_file_path):
                 try:
                     shutil.copy2(file_path, backup_file_path)
-                    cprint("Backups created successfully.", color="green")
+                    cprint(f"Backup created successfully for the file {file_path}.", color="green")
                 except PermissionError:
                     cprint(
                         f"Permission denied when trying to create backup for {file_path}.",
