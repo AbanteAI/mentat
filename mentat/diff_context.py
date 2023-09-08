4cb87fabc00e7e75377579953190b52788baec59
granawkins <granthawkins88@gmail.com>
2023-09-08 09:24:53 +0700

    separate annotate function

--- mentat/diff_context.py
+++ mentat/diff_context.py
@@ -20,7 +20,7 @@ class DiffAnnotation:
     message: List[str]
 
 
-def _parse_diff(diff: List[str]) -> List[dict]:
+def _parse_diff(diff: List[str]) -> List[DiffAnnotation]:
     annotations = []
     active_annotation = None
     for line in diff[4:]:  # Ignore header
@@ -37,9 +37,37 @@ def _parse_diff(diff: List[str]) -> List[dict]:
             if not active_annotation:
                 raise UserError("Invalid diff")
             active_annotation.message.append(line)
+    if active_annotation:
+        annotations.append(active_annotation)
     annotations.sort(key=lambda a: a.start)
     return annotations
 
+def _annotate_file_with_diff(
+    code_message: List[str], 
+    annotations: List[DiffAnnotation]
+) -> List[str]:
+    annotated_message = [
+        code_message[0],  # POSIX path
+    ]
+    active_index = 1
+    for annotation in annotations:
+        if active_index < annotation.start:
+            unaffected_lines = code_message[active_index:annotation.start]
+            annotated_message += unaffected_lines
+        active_index = annotation.start
+        i_minus = 0
+        for line in annotation.message:
+            sign = line[0]
+            if sign == '+':
+                annotated_message.append(f"{active_index}:{line}")
+                active_index += 1
+            elif sign == '-':
+                annotated_message.append(f"{annotation.start + i_minus}:{line}")
+                i_minus += 1
+    if active_index < len(code_message):
+        annotated_message += code_message[active_index:]
+
+    return annotated_message
 
 class DiffContext:
     
@@ -99,22 +127,8 @@ class DiffContext:
         ]
         active_index = 1
         diff = _get_diff_for_file(self.repo, self.base, self.target, rel_path)
+        if not diff:
+            return code_message
         annotations: List[DiffAnnotation] = _parse_diff(diff)
-        for annotation in annotations:
-            if active_index < annotation.start:
-                unaffected_lines = code_message[active_index:annotation.start]
-                annotated_message += unaffected_lines
-            active_index = annotation.start
-            i_minus = 0
-            for line in annotation.message:
-                sign = line[0]
-                if sign == '+':
-                    annotated_message.append(f"{active_index}:{line}")
-                    active_index += 1
-                elif sign == '-':
-                    annotated_message.append(f"{annotation.start + i_minus}:{line}")
-                    i_minus += 1
-        if active_index < len(code_message):
-            annotated_message += code_message[active_index:]
-
-        return annotated_message
+        return _annotate_file_with_diff(code_message, annotations)
+      
