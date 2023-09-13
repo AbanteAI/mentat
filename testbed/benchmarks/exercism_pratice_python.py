291ccb59678708de6f569b7b42011f19553b8942
Jake Koenig <jakethekoenig@gmail.com>
2023-09-13 14:53:41 -0700

    Multithreading and retry on error
    You can now configure how many workers to run with, how many iterations
    to retry with and how many of the exercises to attempt with command line
    flags e.g.
    ```
    pytest -s testbed/benchmarks/exercism_pratice_python.py --num_exercises 200 --max_iterations 2 --max_workers 5
    ```
    I've found one quickly hits rate limits going above 1 though.

--- testbed/benchmarks/exercism_pratice_python.py
+++ testbed/benchmarks/exercism_pratice_python.py
@@ -1,78 +1,142 @@
+import concurrent.futures
 import os
+import subprocess
+import threading
+import time
 
 import pytest
 from git import Repo
-from prompt_toolkit import PromptSession
 
 from mentat.app import run
+from mentat.user_input_manager import UserInputManager, UserQuitInterrupt
+
+threadLocal = threading.local()
+
+
+def exercise_passed():
+    try:
+        with open(threadLocal.test_output_file, "r") as f:
+            lines = f.readlines()
+            return not "failed" in lines[-1] and "passed" in lines[-1]
+    except:
+        return False
+
+
+def get_error_message():
+    with open(threadLocal.test_output_file, "r") as f:
+        lines = f.readlines()
+        if len(lines) > 30:
+            lines = lines[-30:]
+        return "\n".join(lines)
 
 
 @pytest.fixture
-def mock_prompt_session_prompt(mocker):
-    mock_method = mocker.MagicMock()
-    # Mock the super().prompt that MentatPromptSession calls
-    mocker.patch.object(PromptSession, "prompt", new=mock_method)
-    return mock_method
+def mock_user_input_manager(max_iterations, mocker):
+    def mocked_collect_user_input(self, use_plain_session: bool = False) -> str:
+        if not hasattr(self, "call_count"):
+            self.call_count = 0
+        self.call_count += 1
 
+        if self.call_count == 1:
+            return "Please complete the stub program you have been given."
+        elif self.call_count == 2:
+            return "y"
+        else:
+            results = subprocess.run(
+                ["pytest", threadLocal.exercise], stdout=subprocess.PIPE
+            )
+            with open(threadLocal.test_output_file, "w") as f:
+                f.write(results.stdout.decode("utf-8"))
+            if exercise_passed() or threadLocal.iterations >= max_iterations:
+                raise UserQuitInterrupt()
+            if threadLocal.confirm:
+                threadLocal.confirm = False
+                return "y"
+            else:
+                threadLocal.iterations += 1
+                threadLocal.confirm = True
+                return (
+                    "When I ran the test I got the following error message. Can you try again?\n"
+                    + get_error_message()
+                )
 
-exercism_url = "https://github.com/exercism/python.git"
-local_dir = "exercism-python"
+    mocker.patch.object(
+        UserInputManager, "collect_user_input", new=mocked_collect_user_input
+    )
 
 
 @pytest.fixture
 def clone_exercism_python_repo():
+    exercism_url = "https://github.com/exercism/python.git"
+    local_dir = "../exercism-python"
     if os.path.exists(local_dir):
         repo = Repo(local_dir)
         repo.git.reset("--hard")
         repo.remotes.origin.pull()
     else:
         repo = Repo.clone_from(exercism_url, local_dir)
+    # Mentat uses git history so running this test from mentat/ can lead to errors.
+    os.chdir(f"{local_dir}/exercises/practice")
+
+
+@pytest.fixture
+def num_exercises(request):
+    return int(request.config.getoption("--num_exercises"))
+
+
+@pytest.fixture
+def max_iterations(request):
+    return int(request.config.getoption("--max_iterations"))
+
+
+@pytest.fixture
+def max_workers(request):
+    return int(request.config.getoption("--max_workers"))
+
+
+def run_exercise(problem_dir):
+    threadLocal.test_output_file = f"{problem_dir}/test_output.txt"
+    threadLocal.exercise = problem_dir
+    threadLocal.iterations = 1
+    threadLocal.confirm = False
+    time.sleep(0.3)
+    run(
+        [problem_dir],
+        exclude_paths=[
+            f"{problem_dir}/.meta",
+            f"{problem_dir}/.approaches",
+            f"{problem_dir}/.docs",
+            f"{problem_dir}/{problem_dir}_test.py",
+        ],
+        no_code_map=True,
+    )
+    return {
+        "iterations": threadLocal.iterations,
+        "passed": exercise_passed(),
+        "test": problem_dir,
+    }
 
 
 def test_practice_directory_performance(
-    mock_prompt_session_prompt, clone_exercism_python_repo
+    mock_user_input_manager,
+    clone_exercism_python_repo,
+    num_exercises,
+    max_iterations,
+    max_workers,
 ):
-    # Structured this way so it's easy to run on subsets
-    num_to_run = 134
-    passed = 0
-    failed = 0
+    exercises = os.listdir(".")[:num_exercises]
 
-    os.chdir(f"{local_dir}/exercises/practice")
-
-    # TODO: run in parallel
-    for problem_dir in os.listdir("."):
-        if num_to_run == 0:
-            break
-        mock_prompt_session_prompt.side_effect = [
-            "Please complete the stub program you have been given.",
-            "y",
-            KeyboardInterrupt,
-        ]
-        # TODO: change the following line to exclude files that contain the word test. Or do it both ways
-        # TODO: exclude all paths that start with dot
-        run(
-            [problem_dir],
-            exclude_paths=[f"{problem_dir}/.docs", f"{problem_dir}/.meta"],
-            no_code_map=True,
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        results = list(executor.map(run_exercise, exercises))
+        print("Results: ", results)
+        first_iteration = len(
+            [
+                result
+                for result in results
+                if result["iterations"] == 1 and result["passed"]
+            ]
         )
-        test = None
-        for file in os.listdir(problem_dir):
-            if file.endswith("_test.py"):
-                test = file
-                break
-        if test:
-            # TODO: get error and feed back in to give mentat another chance.
-            results = pytest.main([f"{problem_dir}/{test}"])
-            print(results)
-            if results == 0:
-                passed += 1
-            else:
-                failed += 1
-        else:
-            print(f"Could not find test for {problem_dir}")
-        num_to_run -= 1
-
-    # TODO: I'm not sure it makes sense to run with pytest because it's not so much
-    # a pass fail script as something to collect a variety of statistics. I may refactor into a scipt.
-    print(f"Passed: {passed}")
-    print(f"Failed: {failed}")
+        eventually = len([result for result in results if result["passed"]])
+        print(
+            f"Results: {first_iteration}/{num_exercises} passed in the first attempt and {eventually}/{num_exercises} passed in {max_iterations} attempts"
+        )
