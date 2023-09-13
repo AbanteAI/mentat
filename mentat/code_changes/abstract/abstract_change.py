2a7d89c8ef238d8f535ef929d8cb5609702c40b9
Paul Swingle <swingle8paul@gmail.com>
2023-09-13 12:15:51 -0700

    use abstractchanges in codefilemanager instead of regular codechanges

--- mentat/code_changes/abstract/abstract_change.py
+++ mentat/code_changes/abstract/abstract_change.py
@@ -1,14 +1,22 @@
+import logging
 from functools import cmp_to_key
 from pathlib import Path
+from typing import cast
 
 import attr
+from termcolor import cprint
+
+from mentat.code_context import CodeContext
+from mentat.code_file import CodeFile
+from mentat.errors import MentatError
+from mentat.user_input_manager import UserInputManager
 
 
 @attr.s
 class Addition:
     # Will insert directly before this line, 0 indexed
     line_number: int = attr.field()
-    content: str = attr.field()
+    content: list[str] = attr.field()
     owner: int = attr.field()
 
 
@@ -24,7 +32,7 @@ class Deletion:
 @attr.s
 class Rename:
     # None represents deleting this file
-    name: str | None = attr.field()
+    name: Path | None = attr.field()
     owner: int = attr.field()
 
 
@@ -51,15 +59,23 @@ def subchange_order(sub_1: AbstractSubChange, sub_2: AbstractSubChange) -> int:
 
 
 class AbstractChange:
-    def __init__(self, file_path: Path, changes: list[AbstractSubChange]):
+    def __init__(self, file_path: Path | None, changes: list[AbstractSubChange]):
         """
         Creates a new AbstractChange for the given file with the given changes.
         The changes will be applied from the end of the file to the start.
-        Multiple Additions on the same line will give the first Addition in the list priority.
+        Multiple Additions on the same line will give the first Addition in the list priority;
+        this will place it's content later in the file than the other Additions
         A Rename with name = None is a deletion, and a Rename when file_path is None is a file creation.
         """
-        self.file_path = file_path
 
+        self.file_path = file_path
+        self.changes = sorted(
+            self._fix_changes(changes),
+            reverse=True,
+            key=cmp_to_key(subchange_order),
+        )
+
+    def _fix_changes(self, changes: list[AbstractSubChange]):
         deletions = [change for change in changes if type(change) == Deletion]
         additions = [change for change in changes if type(change) == Addition]
         renames = [change for change in changes if type(change) == Rename]
@@ -92,6 +108,7 @@ class AbstractChange:
                 cur_deletion += 1
             if cur_deletion == len(deletions):
                 break
+
             while (
                 cur_addition < len(additions)
                 and additions[cur_addition].line_number
@@ -100,6 +117,7 @@ class AbstractChange:
                 cur_addition += 1
             if cur_addition == len(additions):
                 break
+
             if (
                 additions[cur_addition].line_number
                 > deletions[cur_deletion].starting_line
@@ -110,8 +128,98 @@ class AbstractChange:
                     cur_deletion
                 ].starting_line
 
-        self.changes = sorted(
-            deletions + additions + renames,
-            reverse=True,
-            key=cmp_to_key(subchange_order),
+        return additions + deletions + renames
+
+    def apply(
+        self,
+        code_lines: list[str],
+        code_context: CodeContext,
+        user_input_manager: UserInputManager,
+    ):
+        for subchange in self.changes:
+            if type(subchange) == Rename:
+                self._apply_rename(subchange, code_context, user_input_manager)
+            elif type(subchange) == Addition:
+                self._apply_addition(subchange, code_lines, code_context)
+            elif type(subchange) == Deletion:
+                self._apply_deletion(subchange, code_lines, code_context)
+        if self.file_path is not None:
+            with open(self.file_path) as f:
+                f.write("\n".join(code_lines))
+
+    def _apply_rename(
+        self,
+        subchange: Rename,
+        code_context: CodeContext,
+        user_input_manager: UserInputManager,
+    ):
+        if subchange.name is None and self.file_path is None:
+            raise MentatError("Attempted to delete file with no name")
+        # Deleting a file?
+        elif subchange.name is None:
+            # pyright doesn't recognize that the previous if statement type guards self.file_path
+            abs_path = code_context.config.git_root / cast(Path, self.file_path)
+            if not abs_path.exists():
+                logging.error(f"Path {abs_path} non-existent on delete")
+                return
+            cprint(f"Are you sure you want to delete {self.file_path}?", "red")
+            if user_input_manager.ask_yes_no(default_yes=False):
+                cprint(f"Deleting {self.file_path}...")
+                self._delete_file(abs_path, code_context)
+            else:
+                cprint(f"Not deleting {self.file_path}")
+                return
+        # Creating a new file?
+        elif self.file_path is None:
+            abs_path = code_context.config.git_root / subchange.name
+            if abs_path.exists():
+                logging.error(f"Tried to create file {abs_path} which already exists")
+                return
+            cprint(f"Creating new file {subchange.name}", color="light_green")
+            self._create_file(abs_path, code_context)
+        # Renaming a file?
+        else:
+            orig_abs_path = code_context.config.git_root / self.file_path
+            new_abs_path = code_context.config.git_root / subchange.name
+            if new_abs_path.exists():
+                logging.error(
+                    f"Tried to rename file to {new_abs_path} which already exists"
+                )
+                return
+            self._delete_file(orig_abs_path, code_context)
+            self._create_file(new_abs_path, code_context)
+        self.file_path = subchange.name
+
+    def _apply_addition(
+        self, subchange: Addition, code_lines: list[str], code_context: CodeContext
+    ):
+        code_lines += [""] * (max(0, subchange.line_number - len(code_lines)))
+        code_lines = (
+            code_lines[: subchange.line_number]
+            + subchange.content
+            + code_lines[subchange.line_number :]
         )
+
+    def _apply_deletion(
+        self, subchange: Deletion, code_lines: list[str], code_context: CodeContext
+    ):
+        subchange.ending_line = min(subchange.ending_line, len(code_lines))
+        if subchange.ending_line > subchange.starting_line:
+            code_lines = (
+                code_lines[: subchange.starting_line]
+                + code_lines[subchange.ending_line :]
+            )
+
+    def _create_file(self, abs_path: Path, code_context: CodeContext):
+        logging.info(f"Adding new file {abs_path} to context")
+        code_context.files[abs_path] = CodeFile(abs_path)
+        # create any missing directories in the path
+        abs_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(abs_path, "w") as f:
+            f.write("")
+
+    def _delete_file(self, abs_path: Path, code_context: CodeContext):
+        logging.info(f"Deleting file {abs_path}")
+        if abs_path in code_context.files:
+            del code_context.files[abs_path]
+        abs_path.unlink()
--- mentat/code_file_manager.py
+++ mentat/code_file_manager.py
@@ -1,17 +1,12 @@
 import logging
-import math
 import os
-from collections import defaultdict
 from pathlib import Path
 from typing import Union
 
 from termcolor import cprint
 
-from .change_conflict_resolution import (
-    resolve_insertion_conflicts,
-    resolve_non_insertion_conflicts,
-)
-from .code_change import CodeChange, CodeChangeAction
+from mentat.code_changes.abstract.abstract_change import AbstractChange
+
 from .code_context import CodeContext
 from .code_file import CodeFile
 from .config_manager import ConfigManager
@@ -71,111 +66,29 @@ class CodeFileManager:
 
         return "\n".join(code_message)
 
-    def _add_file(self, abs_path: Path):
-        logging.info(f"Adding new file {abs_path} to context")
-        self.code_context.files[abs_path] = CodeFile(abs_path)
-        # create any missing directories in the path
-        abs_path.parent.mkdir(parents=True, exist_ok=True)
-
-    def _delete_file(self, abs_path: Path):
-        logging.info(f"Deleting file {abs_path}")
-        if abs_path in self.code_context.files:
-            del self.code_context.files[abs_path]
-        abs_path.unlink()
-
-    def _handle_delete(self, delete_change: CodeChange):
-        abs_path = self.config.git_root / delete_change.file
-        if not abs_path.exists():
-            logging.error(f"Path {abs_path} non-existent on delete")
-            return
-
-        cprint(f"Are you sure you want to delete {delete_change.file}?", "red")
-        if self.user_input_manager.ask_yes_no(default_yes=False):
-            cprint(f"Deleting {delete_change.file}...")
-            self._delete_file(abs_path)
-        else:
-            cprint(f"Not deleting {delete_change.file}")
-
-    def _get_new_code_lines(
-        self, rel_path: Path, changes: list[CodeChange]
-    ) -> list[str] | None:
-        if not changes:
-            return []
-        if len(set(map(lambda change: change.file, changes))) > 1:
-            raise Exception("All changes passed in must be for the same file")
-
-        changes = sorted(changes, reverse=True)
-
-        # We resolve insertion conflicts twice because non-insertion conflicts
-        # might move insert blocks outside of replace/delete blocks and cause
-        # them to conflict again
-        changes = resolve_insertion_conflicts(changes, self.user_input_manager, self)
-        changes = resolve_non_insertion_conflicts(changes, self.user_input_manager)
-        changes = resolve_insertion_conflicts(changes, self.user_input_manager, self)
-        if not changes:
-            return []
-
-        new_code_lines = self.file_lines[rel_path].copy()
-        if new_code_lines != self._read_file(rel_path):
-            logging.info(f"File '{rel_path}' changed while generating changes")
-            cprint(
-                f"File '{rel_path}' changed while generating; current file changes"
-                " will be erased. Continue?",
-                color="light_yellow",
-            )
-            if not self.user_input_manager.ask_yes_no(default_yes=False):
-                cprint(f"Not applying changes to file {rel_path}.")
-                return None
-
-        # Necessary in case the model needs to insert past the end of the file
-        last_line = len(new_code_lines) + 1
-        largest_changed_line = math.ceil(changes[0].last_changed_line)
-        if largest_changed_line > last_line:
-            new_code_lines += [""] * (largest_changed_line - last_line)
-
-        min_changed_line = largest_changed_line + 1
-        for change in changes:
-            if change.last_changed_line >= min_changed_line:
-                raise MentatError(f"Change line number overlap in file {change.file}")
-            min_changed_line = change.first_changed_line
-            new_code_lines = change.apply(new_code_lines)
-        return new_code_lines
-
-    def write_changes_to_files(self, code_changes: list[CodeChange]) -> None:
-        file_changes = defaultdict[Path, list[CodeChange]](list)
-        for code_change in code_changes:
-            rel_path = code_change.file
-            abs_path = self.config.git_root / rel_path
-            match code_change.action:
-                case CodeChangeAction.CreateFile:
-                    cprint(f"Creating new file {rel_path}", color="light_green")
-                    self._add_file(abs_path)
-                    with open(abs_path, "w") as f:
-                        f.write("\n".join(code_change.code_lines))
-                case CodeChangeAction.DeleteFile:
-                    self._handle_delete(code_change)
-                case CodeChangeAction.RenameFile:
-                    abs_new_path = self.config.git_root / code_change.name
-                    self._add_file(abs_new_path)
-                    code_lines = self.file_lines[rel_path]
-                    with open(abs_new_path, "w") as f:
-                        f.write("\n".join(code_lines))
-                    self._delete_file(abs_path)
-                    file_changes[code_change.name] += file_changes[rel_path]
-                    file_changes[rel_path] = []
-                    self.file_lines[code_change.name] = self._read_file(
-                        code_change.name
-                    )
-                case _:
-                    file_changes[rel_path].append(code_change)
-
-        for rel_path, changes in file_changes.items():
-            abs_path = self.config.git_root / rel_path
-            new_code_lines = self._get_new_code_lines(rel_path, changes)
-            if new_code_lines:
+    def write_changes_to_files(self, code_changes: list[AbstractChange]) -> None:
+        for change in code_changes:
+            if change.file_path is not None:
+                abs_path = self.config.git_root / change.file_path
                 if abs_path not in self.code_context.files:
                     raise MentatError(
                         f"Attempted to edit file {abs_path} not in context"
                     )
-                with open(abs_path, "w") as f:
-                    f.write("\n".join(new_code_lines))
+                elif not abs_path.exists():
+                    raise MentatError(f"Attempted to edit non-existent file {abs_path}")
+
+                code_lines = self.file_lines[change.file_path].copy()
+                if code_lines != self._read_file(change.file_path):
+                    logging.info(
+                        f"File '{change.file_path}' changed while generating changes"
+                    )
+                    cprint(
+                        f"File '{change.file_path}' changed while generating; current"
+                        " file changes will be erased. Continue?",
+                        color="light_yellow",
+                    )
+                    if not self.user_input_manager.ask_yes_no(default_yes=False):
+                        cprint(f"Not applying changes to file {change.file_path}")
+            else:
+                code_lines = []
+            change.apply(code_lines, self.code_context, self.user_input_manager)
