ab29e71007b01242b56e38e105d79c6c579ca2b4
Jake Koenig <jakethekoenig@gmail.com>
2023-09-18 10:48:00 -0700

    Use existing conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,6 +25,31 @@ def filter_mark(items, mark, exists):
 def pytest_addoption(parser):
     parser.addoption("--benchmark", action="store_true")
     parser.addoption("--uitest", action="store_true")
+    # The following flags are used by benchmark tests
+    parser.addoption(
+        "--max_exercises",
+        action="store",
+        default="1",
+        help="The maximum number of exercises to run",
+    )
+    parser.addoption(
+        "--max_iterations",
+        action="store",
+        default="1",
+        help="Number of times to rerun mentat with error messages",
+    )
+    parser.addoption(
+        "--max_workers",
+        action="store",
+        default="1",
+        help="Number of workers to use for multiprocessing",
+    )
+    parser.addoption(
+        "--refresh_repo",
+        action="store_true",
+        default=False,
+        help="When set local changes will be discarded.",
+    )
 
 
 def pytest_configure(config):
