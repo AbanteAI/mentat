44e2d478252a87f04b90f496ef9b478e8c0bc87c
Wayde Gilliam <waydegilliam@gmail.com>
2023-10-05 02:59:45 -0700

    add util func for async subprocess (#124)
    * add util func for async subprocess
    
    * rename
    
    ---------
    
    Co-authored-by: granawkins <granthawkins88@gmail.com>

---
+++ mentat/utils.py
@@ -0,0 +1,19 @@
+import asyncio
+
+
+async def run_subprocess_async(*args: str) -> str:
+    process = await asyncio.create_subprocess_exec(
+        *args,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+        start_new_session=True,
+    )
+    stdout, stderr = await process.communicate()
+
+    if process.returncode != 0:
+        error_output = stderr.decode("utf-8").strip() if stderr else ""
+        raise Exception(f"Subprocess failed with error: {error_output}")
+
+    output = stdout.decode("utf-8").strip() if stdout else ""
+
+    return output
