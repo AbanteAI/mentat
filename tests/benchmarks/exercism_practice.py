9a44e1454fff127ee68bafd692e6d786c67754f1
Paul Swingle <swingle8paul@gmail.com>
2023-10-06 12:39:03 -0700

    clean up benchmark, store results in json file

--- tests/benchmarks/exercism_practice.py
+++ tests/benchmarks/exercism_practice.py
@@ -1,7 +1,7 @@
 import asyncio
+import json
 import os
 import subprocess
-import sys
 from functools import partial
 from multiprocessing import Pool
 from pathlib import Path
@@ -19,18 +19,19 @@ pytestmark = pytest.mark.benchmark
 
 
 def exercise_passed(test_output_file, language):
-    try:
-        with open(test_output_file, "r") as f:
-            lines = f.readlines()
-            if language == "python":
-                return "failed" not in lines[-1] and "passed" in lines[-1]
-            else:
-                return "FAIL" not in lines[0] and "PASS" in lines[0]
-    except FileNotFoundError:
+    if not test_output_file.exists():
         return False
+    with open(test_output_file, "r") as f:
+        lines = f.readlines()
+        if language == "python":
+            return "failed" not in lines[-1] and "passed" in lines[-1]
+        else:
+            return "FAIL" not in lines[0] and "PASS" in lines[0]
 
 
 def get_error_message(test_output_file):
+    if not test_output_file.exists():
+        return False
     with open(test_output_file, "r") as f:
         lines = f.readlines()
         lines = lines[:50]
@@ -59,6 +60,7 @@ def run_exercise_test(exercise, test_output_file, language):
 
 def get_exercise_file(problem_dir, language):
     problem_file = problem_dir
+
     match language:
         case "python":
             file_ext = "py"
@@ -121,89 +123,156 @@ def language(request):
     return request.config.getoption("--language")
 
 
-async def run_exercise(problem_dir, language="python", max_iterations=2):
+prompt = dedent("""\
+    You are a professional code reviewer who helps other coders improve their skills.
+    You recently assigned a coder a small coding test to assess their level, with a pre-written stub template
+    and an automated test suite to determine if they succeeded.
+    They failed the test; using the instructions for the test,
+    the code they wrote for the test, and the output of the test suite,
+    your job is to determine why they failed. Give a terse but informative couple of sentences
+    on why it failed, before giving the reason it failed on the final line in the format
+    reason: <reason_failed>
+    Your response will be parsed programmatically, so you MUST follow the format for the final line!
+    The possible responses for the final line and what they mean are as follows:
+    blank (the coder didn't change the file at all from the stub you provided them)
+    wording (everything was correct, but the coder messed up the wording or spacing which caused it to be rejected)
+    duplication (the coder had a random duplicated line that caused the code to not be compiled/interpreted)
+    syntax (the coder messed up their syntax, meaning their code couldn't be compiled/interpreted)
+    logic (the coder messed up the logic)
+    other (some other reason caused it to fail)""")
+model = "gpt-4-0314"
+
+
+async def failure_analysis(problem_dir, language):
+    setup_api_key()
+    exercise = Path(f"exercises/practice/{problem_dir}")
+
+    # Get instructions
+    docs_path = exercise / ".docs"
+    instructions = ""
+    for file_name in os.listdir(docs_path):
+        with open(docs_path / file_name) as f:
+            contents = f.read()
+            instructions += f"{file_name}\n{contents}\n"
+
+    # Get code
+    code = ""
+    exercise_file = get_exercise_file(problem_dir, language)
+    with open(exercise / exercise_file) as f:
+        contents = f.read()
+        code += f"{exercise_file}\n{contents}"
+
+    # Get test results
+    test_results = ""
+    with open(exercise / "test_output.txt") as f:
+        contents = f.read()
+        test_results = f"test_output.txt\n{contents}"
+
+    # Ask GPT
+    final_message = (
+        f"All instructions:\n{instructions}\nCode to review:\n{code}\nTest"
+        f" results:\n{test_results}"
+    )
+    messages = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": final_message},
+    ]
+    response = ""
+    async for chunk in await call_llm_api(messages, model):
+        content = chunk["choices"][0]["delta"].get("content", "")
+        response += content
+
     try:
-        exercise = Path(f"exercises/practice/{problem_dir}")
-        exercise_file = exercise / get_exercise_file(problem_dir, language)
-        test_output_file = f"{exercise}/test_output.txt"
-        if os.path.exists(test_output_file):
-            passed = exercise_passed(test_output_file, language)
-            return {
-                "iterations": None,
-                "passed": passed,
-                "test": problem_dir,
-            }
+        reason = response.strip().split("\n")[-1].split()[1]
+    except IndexError:
+        reason = "error"
+    return response, reason
 
-        session = await Session.create(
-            paths=[
-                exercise_file,
-                exercise / ".docs",
-            ],
-            exclude_paths=[exercise / ".docs/hints.md"],
-            no_code_map=True,
-        )
-        asyncio.ensure_future(session.start())
 
-        input_request_message = await session.stream.recv("input_request")
-        await session.stream.send(
-            dedent(
-                f"""\
-                    Use the instructions in exercises/practice/{problem_dir} to modify \
-                    {exercise_file}. Keep and implement the existing function or class stubs, they will be \
-                    called from unit tests. Only use standard libraries, don't suggest installing any packages."""
-            ),
-            source=StreamMessageSource.CLIENT,
-            channel=f"input_request:{input_request_message.id}",
+async def record_results(problem_dir, language, result):
+    if not result["passed"]:
+        response, reason = await failure_analysis(problem_dir, language)
+        result["response"] = response
+        result["reason"] = reason
+    results_dir = Path("results")
+    os.makedirs(results_dir, exist_ok=True)
+    with open(results_dir / (problem_dir + ".json"), "w") as f:
+        json.dump(result, f)
+
+
+async def send_message(session, message, input_request_message):
+    await session.stream.send(
+        message,
+        source=StreamMessageSource.CLIENT,
+        channel=f"input_request:{input_request_message.id}",
+    )
+
+
+async def run_exercise(problem_dir, language="python", max_iterations=2):
+    exercise = Path(f"exercises/practice/{problem_dir}")
+    exercise_file = exercise / get_exercise_file(problem_dir, language)
+    test_output_file = exercise / "test_output.txt"
+    session = await Session.create(
+        paths=[
+            exercise_file,
+            exercise / ".docs",
+        ],
+        exclude_paths=[exercise / ".docs/hints.md"],
+        no_code_map=True,
+    )
+    asyncio.ensure_future(session.start())
+    input_request_message = await session.stream.recv("input_request")
+
+    prompt_1 = dedent(
+        f"""\
+        Use the instructions in exercises/practice/{problem_dir} to modify \
+        {exercise_file}. Keep and implement the existing function or class stubs, they will be \
+        called from unit tests. Only use standard libraries, don't suggest installing any packages."""
+    )
+    prompt_2 = dedent(f"""\
+        See the testing errors above.
+        The tests are correct.
+        Fix the code in {exercise_file} to resolve the errors.""")
+
+    iterations = 0
+    while iterations < max_iterations:
+        if exercise_passed(test_output_file, language):
+            break
+        message = (
+            prompt_1
+            if iterations == 0
+            else get_error_message(test_output_file) + prompt_2
         )
+        await send_message(session, message, input_request_message)
         input_request_message = await session.stream.recv("input_request")
-        await session.stream.send(
-            "y",
-            source=StreamMessageSource.CLIENT,
-            channel=f"input_request:{input_request_message.id}",
-        )
+        await send_message(session, "y", input_request_message)
         input_request_message = await session.stream.recv("input_request")
-        iterations = 1
+
         run_exercise_test(exercise, test_output_file, language)
-        while iterations < max_iterations:
-            if exercise_passed(test_output_file, language):
-                break
-            await session.stream.send(
-                get_error_message(test_output_file) + dedent(f"""
-                        See the testing errors above.
-                        The tests are correct.
-                        Fix the code in {exercise_file} to resolve the errors."""),
-                source=StreamMessageSource.CLIENT,
-                channel=f"input_request:{input_request_message.id}",
-            )
-            input_request_message = await session.stream.recv("input_request")
-            await session.stream.send(
-                "y",
-                source=StreamMessageSource.CLIENT,
-                channel=f"input_request:{input_request_message.id}",
-            )
-            input_request_message = await session.stream.recv("input_request")
-            run_exercise_test(exercise, test_output_file, language)
-            iterations += 1
+        iterations += 1
 
-        await session.stop()
-        passed = exercise_passed(test_output_file, language)
-        return {
-            "iterations": iterations,
-            "passed": passed,
-            "test": problem_dir,
-        }
-    except Exception as e:
-        sys.__stdout__.write(f"\nError running {problem_dir}")
-        sys.__stdout__.write(str(e))
-        return {
-            "iterations": iterations,
-            "passed": False,
-            "test": problem_dir,
-        }
+    await session.stop()
+    passed = exercise_passed(test_output_file, language)
+    result = {
+        "iterations": iterations,
+        "passed": passed,
+        "test": problem_dir,
+    }
+    await record_results(problem_dir, language, result)
+    return result
 
 
 def run_exercise_sync(problem_dir, language="python", max_iterations=2):
-    return asyncio.run(run_exercise(problem_dir, language, max_iterations))
+    try:
+        return asyncio.run(run_exercise(problem_dir, language, max_iterations))
+    except Exception as e:
+        print(f"\nError running {problem_dir}")
+        print(str(e), flush=True)
+        return {
+            "iterations": None,
+            "passed": False,
+            "test": problem_dir,
+        }
 
 
 def summarize_results(results):
@@ -255,58 +324,3 @@ def test_practice_directory_performance(
                 summarize_results(results) + "| Last Ran: " + result["test"]
             )
         print(f"Results: {results}")
-
-    # Ask GPT about why it failed
-    setup_api_key()
-    prompt = dedent("""\
-        You are a professional code reviewer who helps other coders improve their skills.
-        You recently assigned a coder a small coding test to assess their level, with a pre-written stub template
-        and an automated test suite to determine if they succeeded. 
-        They failed the test; using the instructions for the test, 
-        the code they wrote for the test, and the output of the test suite, 
-        your job is to determine why they failed. Give a terse but informative couple of sentences
-        on why it failed, before giving the reason it failed on the final line in the format
-        reason: <reason_failed>
-        Your response will be parsed programmatically, so you MUST follow the format for the final line!
-        The possible responses for the final line and what they mean are as follows:
-        blank (the coder didn't change the file at all from the stub you provided them)
-        wording (everything was correct, but the test suite expected a different string to be printed/thrown)
-        duplication (the coder had a random duplicated line that caused the code to not be compiled/interpreted)
-        syntax (the coder messed up their syntax, meaning their code couldn't be compiled/interpreted)
-        logic (the coder messed up the logic)
-        other (some other reason caused it to fail)""")
-    model = "gpt-4-0314"
-    for problem_dir in exercises:
-        exercise = Path(f"exercises/practice/{problem_dir}")
-        docs_path = exercise / ".docs"
-        instructions = ""
-        for file_name in os.listdir(docs_path):
-            with open(docs_path / file_name) as f:
-                contents = f.read()
-                instructions += f"{file_name}\n{contents}\n"
-        code = ""
-        exercise_file = get_exercise_file(problem_dir, language)
-        with open(exercise / exercise_file) as f:
-            contents = f.read()
-            code += f"{exercise_file}\n{contents}"
-
-        test_results = ""
-        with open(exercise / "test_output.txt") as f:
-            contents = f.read()
-            test_results = f"test_output.txt\n{contents}"
-
-        final_message = (
-            f"All instructions:\n{instructions}\nCode to review:\n{code}\nTest"
-            f" results:\n{test_results}"
-        )
-        messages = [
-            {"role": "system", "content": prompt},
-            {"role": "user", "content": final_message},
-        ]
-        response = ""
-        async for chunk in await call_llm_api(messages, model):
-            content = chunk["choices"][0]["delta"].get("content", "")
-            response += content
-
-        print(problem_dir)
-        print(response)
