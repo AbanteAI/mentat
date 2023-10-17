a5b1af8cc170406bad3b7281e4858c227841607a
Paul Swingle <swingle8paul@gmail.com>
2023-10-17 13:03:04 -0700

    fix circular import, fix tests

--- mentat/session_input.py
+++ mentat/session_input.py
@@ -3,38 +3,42 @@ import logging
 import shlex
 from typing import Any, Coroutine
 
-from mentat.commands import Command
-
 from .errors import RemoteKeyboardInterrupt, SessionExit
 from .session_stream import SESSION_STREAM, StreamMessage
 
 
-async def collect_user_input(**kwargs: Any) -> StreamMessage:
+async def _get_input_request(**kwargs: Any) -> StreamMessage:
+    stream = SESSION_STREAM.get()
+
+    message = await stream.send("", channel="input_request", **kwargs)
+    response = await stream.recv(f"input_request:{message.id}")
+    return response
+
+
+async def collect_user_input(plain: bool = False) -> StreamMessage:
     """Listens for user input on a new channel
 
     send a message requesting user to send a response
     create a new broadcast channel that listens for the input
     close the channel after receiving the input
     """
-    stream = SESSION_STREAM.get()
+    # This fixes a circular import
+    from mentat.commands import Command
 
-    message = await stream.send("", channel="input_request", **kwargs)
-    response = await stream.recv(f"input_request:{message.id}")
-
+    response = await _get_input_request(plain=plain)
     logging.debug(f"User Input: {response.data}")
 
     # Intercept and run commands
-    if not kwargs.get("plain"):
+    if not plain:
         while isinstance(response.data, str) and response.data.startswith("/"):
-            arguments = shlex.split(message.data[1:])
+            arguments = shlex.split(response.data[1:])
             command = Command.create_command(arguments[0])
             await command.apply(*arguments[1:])
 
-            message = await stream.send("", channel="input_request", **kwargs)
-            response = await stream.recv(f"input_request:{message.id}")
+            response = await _get_input_request(plain=plain)
 
     # Quit on q
-    if response.data == "q":
+    if isinstance(response.data, str) and response.data.strip() == "q":
         raise SessionExit
 
     return response
