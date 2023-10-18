c212d1a467377705da8fab0128ca108ede84c515
Paul Swingle <swingle8paul@gmail.com>
2023-10-18 11:19:00 -0700

    fix running mentat from a sub folder and also fix duplicated code features bug

--- tests/code_context_test.py
+++ tests/code_context_test.py
@@ -277,9 +277,8 @@ async def test_get_code_message_include(
         return count_tokens(code_message, "gpt-4")
 
     # If max_tokens is None, include the full auto-context
-    if not code_context.settings.no_code_map:
-        assert await _count_auto_tokens_where(None) == 288  # Cmap w/ signatures
-        assert await _count_auto_tokens_where(250) == 220  # Cmap
+    assert await _count_auto_tokens_where(None) == 236  # Cmap w/ signatures
+    assert await _count_auto_tokens_where(250) == 220  # Cmap
     assert await _count_auto_tokens_where(200) == 153  # fnames
     # Always return include_files, regardless of max
     assert await _count_auto_tokens_where(0) == 102  # Include_files only
