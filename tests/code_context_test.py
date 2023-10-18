8fbc302f1d596be3d98d12dad5195970c9805362
Paul Swingle <swingle8paul@gmail.com>
2023-10-18 11:44:20 -0700

    fix tests

--- tests/code_context_test.py
+++ tests/code_context_test.py
@@ -278,7 +278,7 @@ async def test_get_code_message_include(
 
     # If max_tokens is None, include the full auto-context
     assert await _count_auto_tokens_where(None) == 236  # Cmap w/ signatures
-    assert await _count_auto_tokens_where(250) == 220  # Cmap
-    assert await _count_auto_tokens_where(200) == 153  # fnames
+    assert await _count_auto_tokens_where(230) == 184  # Cmap
+    assert await _count_auto_tokens_where(170) == 134  # fnames
     # Always return include_files, regardless of max
     assert await _count_auto_tokens_where(0) == 102  # Include_files only
