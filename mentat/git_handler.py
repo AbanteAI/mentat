b1196e61ae9c054c29e816f65c5b674980e84de3
Jake Koenig <jakethekoenig@gmail.com>
2023-10-30 10:39:29 -0700

    git_root not optional

--- mentat/git_handler.py
+++ mentat/git_handler.py
@@ -124,11 +124,7 @@ def get_diff_for_file(target: str, path: Path) -> str:
         raise UserError()
 
 
-def get_treeish_metadata(target: str, git_root: Path | None = None) -> dict[str, str]:
-    if git_root is None:
-        session_context = SESSION_CONTEXT.get()
-        git_root = session_context.git_root
-
+def get_treeish_metadata(git_root: Path, target: str) -> dict[str, str]:
     try:
         commit_info = subprocess.check_output(
             ["git", "log", target, "-n", "1", "--pretty=format:%H %s"],
