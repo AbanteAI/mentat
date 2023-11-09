4fea6ec4882121f3f08dea729b50ceea3fd6fdb8
PCSwingle <swingle8paul@gmail.com>
2023-11-09 11:08:06 -0700

    remove split diff (#259)

--- mentat/parsers/parser_map.py
+++ mentat/parsers/parser_map.py
@@ -1,12 +1,10 @@
 from mentat.parsers.block_parser import BlockParser
 from mentat.parsers.parser import Parser
 from mentat.parsers.replacement_parser import ReplacementParser
-from mentat.parsers.split_diff_parser import SplitDiffParser
 from mentat.parsers.unified_diff_parser import UnifiedDiffParser
 
 parser_map: dict[str, Parser] = {
     "block": BlockParser(),
     "replacement": ReplacementParser(),
-    "split-diff": SplitDiffParser(),
     "unified-diff": UnifiedDiffParser(),
 }
