9701f51805150b500e976dc264daccd42c36d77c
BioBootloader <thebiobootloader@gmail.com>
2023-11-10 16:13:58 -0800

    version -> 0.1.20

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(readme_path, "r", encoding="utf-8") as f:
 
 setup(
     name="mentat",
-    version="0.1.19",
+    version="0.1.20",
     python_requires=">=3.10",
     packages=find_packages(include=["mentat", "mentat.*"]),
     install_requires=[
