2e76c11b3b9bba6afd5d8a8354fd75ca27683470
PCSwingle <swingle8paul@gmail.com>
2023-11-27 10:09:34 -0700

    specify to only use pip if installed with pip (#304)

--- mentat/utils.py
+++ mentat/utils.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import hashlib
-import sys
 import time
 from importlib import resources
 from importlib.abc import Traversable
@@ -109,11 +108,11 @@ def check_version():
             latest_version
         ):
             ctx.stream.send(
-                f"Version v{latest_version} of mentat is available. To upgrade, run:",
+                f"Version v{latest_version} of Mentat is available. If pip was used to"
+                " install Mentat, upgrade with:",
                 color="light_red",
             )
-            py = sys.executable
-            ctx.stream.send(f"{py} -m pip install --upgrade mentat", color="yellow")
+            ctx.stream.send("pip install --upgrade mentat", color="yellow")
     except Exception as err:
         ctx.stream.send(f"Error checking for most recent version: {err}", color="red")
 
