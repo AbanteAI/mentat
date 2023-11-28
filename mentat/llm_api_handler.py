b0366fdf96ec39cf171782893471e6b55d44c1be
Jake Koenig <jakethekoenig@gmail.com>
2023-11-28 14:25:21 -0800

    Adjusted current gpt-3.5-price

--- mentat/llm_api_handler.py
+++ mentat/llm_api_handler.py
@@ -136,7 +136,7 @@ def model_price_per_1000_tokens(model: str) -> Optional[tuple[float, float]]:
         "gpt-4-0314": (0.03, 0.06),
         "gpt-4-32k-0314": (0.06, 0.12),
         "gpt-3.5-turbo-1106": (0.001, 0.002),
-        "gpt-3.5-turbo": (0.0015, 0.002),
+        "gpt-3.5-turbo": (0.001, 0.002),
         "gpt-3.5-turbo-0613": (0.0015, 0.002),
         "gpt-3.5-turbo-16k-0613": (0.003, 0.004),
         "gpt-3.5-turbo-0301": (0.0015, 0.002),
