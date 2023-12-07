6296b784297f0aa0ddde6046b02cd40c6e8635e8
Wayde Gilliam <waydegilliam@gmail.com>
2023-12-07 14:22:14 -0800

    Run isort

--- tests/diff_context_test.py
+++ tests/diff_context_test.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import pytest
 
 from mentat.diff_context import DiffContext
-from mentat.session_context import SESSION_CONTEXT
 from mentat.python_client.client import PythonClient
+from mentat.session_context import SESSION_CONTEXT
 
 
 def _update_ops(temp_testbed, last_line, commit_message=None):
