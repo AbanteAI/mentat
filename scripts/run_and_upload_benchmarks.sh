ba142833737678187fd3963ee2443cbe4dc41611
Jake Koenig <jakethekoenig@gmail.com>
2023-12-07 12:40:06 -0800

    Workflow fixes

--- scripts/run_and_upload_benchmarks.sh
+++ scripts/run_and_upload_benchmarks.sh
@@ -3,7 +3,7 @@
 pytest -s tests/benchmarks/exercism_practice.py \
     --max_iterations 2 \
     --max_workers 1 \
-    --max_benchmarks 4 \ # Starting low
+    --max_benchmarks 4 \
     --language javascript \
     --benchmark
 
@@ -13,6 +13,6 @@ aws s3 cp benchmark_repos/exercism-javascript/results.html s3://abante-benchmark
 
 # Send slack notification
 RESULTS_URL="https://abante-benchmark-results.s3.amazonaws.com/exercism-javascript-results-${TIMESTAMP}.html"
-curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"Benchmark results: ${RESULTS_URL}\"}" $SLACK_BENCHMARK_NOTIFICATION_WEBHOOK
+curl -X POST -H "Content-Type: application/json" -d "{\"benchmark_report\": \"${RESULTS_URL}\"}" $SLACK_BENCHMARK_NOTIFICATION_WEBHOOK
 
 
