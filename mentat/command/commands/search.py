2e9f8ef0520f842335305c0a8fbeae04d14e389d
Paul Swingle <swingle8paul@gmail.com>
2023-12-13 15:57:33 -0800

    add include option to /search command

--- mentat/command/commands/search.py
+++ mentat/command/commands/search.py
@@ -1,6 +1,7 @@
 from mentat.command.command import Command
 from mentat.errors import UserError
 from mentat.session_context import SESSION_CONTEXT
+from mentat.utils import get_relative_path
 
 SEARCH_RESULT_BATCH_SIZE = 10
 
@@ -10,6 +11,7 @@ class SearchCommand(Command, command_name="search"):
         session_context = SESSION_CONTEXT.get()
         stream = session_context.stream
         code_context = session_context.code_context
+        config = session_context.config
 
         if len(args) == 0:
             stream.send("No search query specified", color="yellow")
@@ -21,19 +23,57 @@ class SearchCommand(Command, command_name="search"):
             stream.send(str(e), color="red")
             return
 
+        cumulative_tokens = 0
         for i, (feature, score) in enumerate(results, start=1):
+            i_str = f"{i:3}"
+            score_str = f"{score:.3f}"
+
             label = feature.ref()
-            if label.startswith(str(session_context.cwd)):
-                label = label[len(str(session_context.cwd)) + 1 :]
+            label = label.removeprefix(str(session_context.cwd) + "/")
             if feature.name:
                 label += f' "{feature.name}"'
-            stream.send(f"{i:3} | {score:.3f} | {label}")
+
+            tokens = feature.count_tokens(config.model)
+            cumulative_tokens += tokens
+
+            tokens_str = f"{tokens} Tokens"
+            cumulative_tokens_str = f"{cumulative_tokens} Cumulative Tokens"
+
+            message = " | ".join(
+                [i_str, score_str, tokens_str, cumulative_tokens_str, label]
+            )
+            stream.send(message)
             if i > 1 and i % SEARCH_RESULT_BATCH_SIZE == 0:
                 # TODO: Required to avoid circular imports, but not ideal.
-                from mentat.session_input import ask_yes_no
+                from mentat.session_input import collect_user_input
 
-                stream.send("\nShow More results? ")
-                if not await ask_yes_no(default_yes=True):
+                stream.send(
+                    "\nEnter (Y) for more results, (n) to exit search mode, or a number"
+                    " to add all features above that number to context."
+                )
+                user_input: str = (await collect_user_input(plain=True)).data.strip()
+                while not (
+                    (user_input.isdigit() and int(user_input) > 0)
+                    or (user_input.lower() in "yn")
+                ):
+                    stream.send("(Y/n/number)")
+                    user_input: str = (
+                        await collect_user_input(plain=True)
+                    ).data.strip()
+
+                if not user_input or user_input.lower() == "y":
+                    continue
+                elif user_input.lower() == "n":
+                    break
+                else:
+                    index = min(int(user_input), len(results))
+                    for feat, _ in results[:index]:
+                        included_paths = code_context.include(feat.ref())
+                        for included_path in included_paths:
+                            rel_path = get_relative_path(
+                                included_path, session_context.cwd
+                            )
+                            stream.send(f"{rel_path} added to context", color="green")
                     break
 
     @classmethod
