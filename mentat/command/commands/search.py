1ca540cd8f5b6f07b41f4a03f3eb6f7e205c5fce
Paul Swingle <swingle8paul@gmail.com>
2023-12-14 15:55:27 -0800

    change ui for accepting specific numbers

--- mentat/command/commands/search.py
+++ mentat/command/commands/search.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 
 from termcolor import colored
 from typing_extensions import override
@@ -9,6 +9,26 @@ from mentat.session_context import SESSION_CONTEXT
 from mentat.utils import get_relative_path
 
 SEARCH_RESULT_BATCH_SIZE = 5
+
+
+def _parse_include_input(user_input: str, max_num: int) -> Set[int] | None:
+    nums: Set[int] = set()
+    for part in user_input.split():
+        left_right = part.split("-")
+        if len(left_right) > 2:
+            return None
+        elif len(left_right) == 2:
+            if not left_right[0].isdigit() or not left_right[1].isdigit():
+                return None
+            nums.update(range(int(left_right[0]), int(left_right[1]) + 1))
+        else:
+            if not part.isdigit():
+                return None
+            nums.add(int(part))
+    nums = set(num for num in nums if num > 0 and num <= max_num)
+    if not nums:
+        return None
+    return nums
 
 
 class SearchCommand(Command, command_name="search"):
@@ -59,36 +79,30 @@ class SearchCommand(Command, command_name="search"):
                 from mentat.session_input import collect_user_input
 
                 stream.send(
-                    "\nEnter (Y) for more results, (n) to exit search mode, or a number"
-                    " to add all features above that number to context."
+                    "(Y/n) for more results or to exit search mode.\nResults to"
+                    ' include in context: (eg: "1 3 4" or "1-4")'
                 )
                 user_input: str = (await collect_user_input(plain=True)).data.strip()
-                while not (
-                    (
-                        user_input.isdigit()
-                        and int(user_input) > 0
-                        and int(user_input) <= i
-                    )
-                    or (user_input.lower() in "yn")
-                ):
-                    stream.send("(Y/n/number)")
+                while user_input.lower() not in "yn":
+                    to_include = _parse_include_input(user_input, i)
+                    if to_include is not None:
+                        for index in to_include:
+                            feat = results[index - 1][0]
+                            included_paths = code_context.include(feat.ref())
+                            for included_path in included_paths:
+                                rel_path = get_relative_path(
+                                    included_path, session_context.cwd
+                                )
+                                stream.send(
+                                    f"{rel_path} added to context", color="green"
+                                )
+                    else:
+                        stream.send("(Y/n)")
                     user_input: str = (
                         await collect_user_input(plain=True)
                     ).data.strip()
-
-                if not user_input or user_input.lower() == "y":
-                    continue
-                elif user_input.lower() == "n":
-                    break
-                else:
-                    index = min(int(user_input), len(results))
-                    for feat, _ in results[:index]:
-                        included_paths = code_context.include(feat.ref())
-                        for included_path in included_paths:
-                            rel_path = get_relative_path(
-                                included_path, session_context.cwd
-                            )
-                            stream.send(f"{rel_path} added to context", color="green")
+                if user_input.lower() == "n":
+                    stream.send("Exiting search mode...", color="light_blue")
                     break
 
     @override
