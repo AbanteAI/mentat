28abc8640439cf20e17d74bfd8c8095531833f0a
Amit Joshi <amit.joshi@ihsmarkit.com>
2023-12-18 15:40:08 -0500

    Reorder the imports to meet isort requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sys
 import os
+import sys
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
