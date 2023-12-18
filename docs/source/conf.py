5f779eeed6a211c973b93eae00cae36e4e851765
Amit Joshi <amit.joshi@ihsmarkit.com>
2023-12-18 15:28:15 -0500

    Fix PR linter failure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,32 +9,31 @@ import os
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Mentat'
-copyright = '2023, Multiple Authors'
-author = 'Multiple Authors'
-release = '1.0.6'
+project = "Mentat"
+copyright = "2023, Multiple Authors"
+author = "Multiple Authors"
+release = "1.0.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-'sphinx.ext.autodoc',
-'sphinx.ext.viewcode',
-'sphinx.ext.napoleon',
-'sphinx.ext.todo'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 
 # AXJ
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
