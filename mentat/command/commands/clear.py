9e08b37b382c1a6f05d87ebb5fbf999390d58c90
Grant <50287275+granawkins@users.noreply.github.com>
2023-12-22 06:06:37 +0700

    Update mentat/command/commands/clear.py
    Co-authored-by: biobootloader <128252497+biobootloader@users.noreply.github.com>

--- mentat/command/commands/clear.py
+++ mentat/command/commands/clear.py
@@ -35,5 +35,5 @@ class ClearCommand(Command, command_name="clear"):
     @classmethod
     def help_message(cls) -> str:
         return (
-            "Clear the current message history and auto included features from context."
+            "Clear the current message history and auto included code features from context."
         )
