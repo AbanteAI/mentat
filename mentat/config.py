e3ff27786c399a2d9b01dcfa73506e17a7323503
A-joshi <amitsharadjoshi@gmail.com>
2024-01-02 12:41:55 -0500

    Change descriptions based on the review

--- mentat/config.py
+++ mentat/config.py
@@ -37,7 +37,7 @@ class Config:
         default="gpt-4-1106-preview",
         metadata={
             "auto_completions": list(known_models.keys()),
-            "description": "Choose from one of the supported models: " + ",".join(
+            "description": "Choose from one of the supported models: " + ", ".join(
                 list(known_models.keys())
             ),
         },
@@ -47,8 +47,10 @@ class Config:
         metadata={
             "auto_completions": list(known_models.keys()),
             "description": (
-                "Choose from one of the supported feature selection models: "
-                + ",".join(list(known_models.keys()))
+                "The model used to prune the features before sending to the main model"
+                " for edits. Only used if llm_feature_filter is greater than 0."
+                " Possible models are: "
+                + ", ".join(list(known_models.keys()))
             ),
         },
     )
@@ -60,7 +62,7 @@ class Config:
             ],
             "description": (
                 "Choose from one of the supported embedding models: "
-                + ",".join(
+                + ", ".join(
                     [
                         model.name
                         for model in known_models.values()
@@ -76,9 +78,9 @@ class Config:
         validator=[validators.le(1), validators.ge(0)],
         metadata={
             "description": (
-                "Temprature controls the model variability from the context: higher"
-                " value implies more variability. Teemprature is a floating point"
-                " number between 0 and 1. "
+                "The temperature setting used by model. Temprature is a floating point"
+                " number that controls the model variability from the context: higher"
+                " value implies more variability. number between 0 and 1. "
             )
         },
     )
