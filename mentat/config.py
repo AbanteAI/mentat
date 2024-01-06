2cba43c50592159d621ec0745457e24127be7637
granawkins <granthawkins88@gmail.com>
2024-01-06 13:00:17 +0700

    change default model to finetuned 3.5 (temporarily)

--- mentat/config.py
+++ mentat/config.py
@@ -35,7 +35,7 @@ class Config:
 
     # Model specific settings
     model: str = attr.field(
-        default="gpt-4-1106-preview",
+        default="ft:gpt-3.5-turbo-1106:abante::8dsQMc4F",
         metadata={"auto_completions": list(known_models.keys())},
     )
     feature_selection_model: str = attr.field(
