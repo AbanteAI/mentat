1aaba8c81c2b8fa67fa25a1b5325d5cbf016248c
granawkins <granthawkins88@gmail.com>
2024-01-19 07:19:48 +0700

    ignore module import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ import sys
 
 # AXJ, Version
 sys.path.insert(0, os.path.abspath("../.."))
-from mentat import __version__
+from mentat import __version__  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
