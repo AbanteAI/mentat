520d47ff878a40e04c09f95c8a81f2b931878089
granawkins <granthawkins88@gmail.com>
2024-01-21 10:13:04 +0700

    implement basic call graph

--- mentat/treesitter/parser.py
+++ mentat/treesitter/parser.py
@@ -1,6 +1,9 @@
-from dataclasses import dataclass
+from __future__ import annotations
+
+from collections import defaultdict
 from pathlib import Path
 
+import attrs
 from tree_sitter import Language, Node, Parser
 
 file_extension_map = {
@@ -10,74 +13,113 @@ file_extension_map = {
 }
 
 
-@dataclass
-class FunctionSignature:
-    name: str
-    return_type: str
-    parameters: str
-
-
 class TreesitterParsingError(Exception):
     """
     Raised when an error is encountered parsing a file with treesitter, typically ignored
     """
 
 
-def parse_node(node: Node, source_code: str) -> FunctionSignature | None:
+@attrs.define
+class CallGraphNode:
+    name: str
+    return_type: str
+    parameters: str
+
+
+class CallGraph:
+    def __init__(self):
+        self.nodes = dict[str, CallGraphNode]()
+        self.edges: dict[str, list[str]] = defaultdict(list)
+
+    def add_node(self, node: CallGraphNode):
+        self.nodes[node.name] = node
+
+    def add_edge(self, _from: str, _to: str):
+        self.edges[_from].append(_to)
+
+    def save(self, path: Path):
+        with open(path, "w") as f:
+            f.write("NODES\n")
+            f.write("\n".join(self.nodes.keys()))
+            f.write("\nEDGES\n")
+            f.write("\n".join([f"{k} -> {v}" for k, v in self.edges.items()]))
+
+
+def parse_node(
+    node: Node, call_path: str, call_graph: CallGraph | None = None
+) -> CallGraph:
     """Extracts function signature data from a given node."""
+    if call_graph is None:
+        call_graph = CallGraph()
+
+    def _clean_text(node: Node) -> str:
+        return " ".join(node.text.decode("utf-8").split())
+
+    def _clean_field(node: Node, field: str) -> str:
+        _field = node.child_by_field_name(field)
+        return _clean_text(_field) if _field else ""
+
+    # If it's a function_definition, add a node and add to call_path
     if node.type == "function_definition":
-
-        def _clean_text(node: Node) -> str:
-            return " ".join(node.text.decode("utf-8").split())
-
-        _name = node.child_by_field_name("name")
-        _return_type = node.child_by_field_name("return_type")
-        _parameters = node.child_by_field_name("parameters")
-        return FunctionSignature(
-            _clean_text(_name) if _name else "",
-            _clean_text(_return_type) if _return_type else "",
-            _clean_text(_parameters) if _parameters else "",
+        call_path = call_path + ":" + _clean_field(node, "name")
+        _return_type = _clean_field(node, "return_type")
+        _parameters = _clean_field(node, "parameters")
+        new_node = CallGraphNode(
+            call_path,
+            _return_type,
+            _parameters,
         )
-    else:
-        return None
+        call_graph.add_node(new_node)
+
+    # Else if it's a call, add an edge to call_path
+    elif node.type == "call":
+        _name = _clean_field(node, "function")
+        call_graph.add_edge(call_path, _name)
+
+    # If it has children, parse with call_path
+    if node.children:
+        for child in node.children:
+            # if "calculate" in call_path and int(child.start_point[0]) >= 8:
+            #     print('wait')
+            parse_node(child, call_path, call_graph)
+
+    return call_graph
 
 
-def parse_file(path: Path) -> list[FunctionSignature]:
+def parse_file(path: Path, cwd: Path, cg: CallGraph | None = None) -> CallGraph:
     # Load parser
     filetype = path.suffix
     if filetype not in file_extension_map:
         raise TreesitterParsingError(f"Filetype {filetype} not supported")
+
+    if cg is None:
+        cg = CallGraph()
+    relative_path = path.relative_to(cwd)
+    call_path = relative_path.as_posix()
+
     language_name = file_extension_map[filetype]
     language = Language(str(Path(__file__).parent / "ts-lang.so"), language_name)
     parser = Parser()
     parser.set_language(language)
-    # Create syntax tree
     source_code = path.read_text()
     tree = parser.parse(bytes(source_code, "utf8"))
-    functions = list[FunctionSignature]()
     cursor = tree.walk()
     cursor.goto_first_child()
     while True:
-        function_data = parse_node(cursor.node, source_code)
-        if function_data:
-            functions.append(function_data)
+        parse_node(cursor.node, call_path, cg)
         if not cursor.goto_next_sibling():
             break
+    return cg
 
-    return functions
 
-
-def parse_dir(
-    path: Path, cwd: Path | str | None = None
-) -> dict[str, list[FunctionSignature]]:
-    dir_functions = dict[str, list[FunctionSignature]]()
+def parse_dir(path: Path, cwd: Path | str | None = None) -> CallGraph:
+    cwd = path if cwd is None else Path(cwd)
+    cg = CallGraph()
     for file in path.iterdir():
-        relative_path = file.relative_to(cwd) if cwd else file
         try:
             if file.is_file():
-                file_functions = parse_file(file)
-                dir_functions[relative_path.as_posix()] = file_functions
+                parse_file(file, cwd, cg)
         except TreesitterParsingError:
             print(f"Skipping {file} due to parsing error")
             continue
-    return dir_functions
+    return cg
--- tests/treesitter_test.py
+++ tests/treesitter_test.py
@@ -1,23 +1,50 @@
-from mentat.treesitter.parser import FunctionSignature, parse_dir, parse_file
+from pathlib import Path
+from mentat.treesitter.parser import parse_dir, parse_file
 
 
 def test_parse_file(temp_testbed):
     test_path = temp_testbed / "multifile_calculator" / "operations.py"
-    file_functions = parse_file(test_path)
-    assert file_functions == [
-        FunctionSignature(name="add_numbers", return_type="", parameters="(a, b)"),
-        FunctionSignature(name="multiply_numbers", return_type="", parameters="(a, b)"),
-        FunctionSignature(name="subtract_numbers", return_type="", parameters="(a, b)"),
-        FunctionSignature(name="divide_numbers", return_type="", parameters="(a, b)"),
-    ]
+    call_graph = parse_file(test_path, cwd=temp_testbed)
+    assert sorted(list(call_graph.nodes.keys())) == sorted(
+        [
+            "multifile_calculator/operations.py:add_numbers",
+            "multifile_calculator/operations.py:multiply_numbers",
+            "multifile_calculator/operations.py:subtract_numbers",
+            "multifile_calculator/operations.py:divide_numbers",
+        ]
+    )
+    assert dict(call_graph.edges) == {}
 
 
 def test_parse_dir(temp_testbed):
     test_path = temp_testbed / "multifile_calculator"
-    tree = parse_dir(test_path, cwd=temp_testbed)
-    dir_function_keys = [
-        "multifile_calculator/__init__.py",
-        "multifile_calculator/calculator.py",
-        "multifile_calculator/operations.py",
+    call_graph = parse_dir(test_path, cwd=temp_testbed)
+    assert sorted(list(call_graph.nodes.keys())) == sorted(
+        [
+            "multifile_calculator/operations.py:add_numbers",
+            "multifile_calculator/operations.py:multiply_numbers",
+            "multifile_calculator/operations.py:subtract_numbers",
+            "multifile_calculator/operations.py:divide_numbers",
+            "multifile_calculator/calculator.py:calculate",
+        ]
+    )
+    assert call_graph.edges["multifile_calculator/calculator.py:calculate"] == [
+        "add_numbers",
+        "subtract_numbers",
+        "multiply_numbers",
+        "divide_numbers",
+        "print",
     ]
-    assert sorted(list(tree.keys())) == sorted(dir_function_keys)
+    assert call_graph.edges["multifile_calculator/calculator.py"] == [
+        "fire.Fire",
+    ]
+
+
+def test_call_graph_on_mentat(temp_testbed):
+    mentat_path = Path(__file__).parent.parent / "mentat"
+    call_graph = parse_dir(mentat_path)
+    call_graph.save(temp_testbed / "call_graph.txt")
+
+    saved_call_graph = (temp_testbed / "call_graph.txt").read_text()
+    assert saved_call_graph.startswith("NODES\n")
+    assert "\nEDGES\n" in saved_call_graph
