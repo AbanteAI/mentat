949cd2ffb3a8decf73accbc7dcd76d2e42e4b46c
Jake Koenig <jakethekoenig@gmail.com>
2024-02-14 07:53:38 -0800

    Release 1.0.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(readme_path, "r", encoding="utf-8") as f:
 
 setup(
     name="mentat",
-    version="1.0.9",
+    version="1.0.10",
     python_requires=">=3.10",
     packages=find_packages(
         include=["mentat", "mentat.*", "benchmarks", "benchmarks.*"]
