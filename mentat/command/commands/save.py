0c9f8aa4a73c11a65329cc71ad09e5394c4e67c3
galer7 <gabigaler50@gmail.com>
2024-02-24 20:18:23 +0200

    Add save command file

---
+++ mentat/command/commands/save.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from typing import List
+
+from typing_extensions import override
+
+from mentat.auto_completer import get_command_filename_completions
+from mentat.command.command import Command, CommandArgument
+from mentat.session_context import SESSION_CONTEXT
+from mentat.utils import mentat_dir_path
+from mentat.errors import PathValidationError
+
+import json
+
+
+class SaveCommand(Command, command_name="save"):
+    @override
+    async def apply(self, *args: str) -> None:
+        session_context = SESSION_CONTEXT.get()
+        stream = session_context.stream
+        code_context = session_context.code_context
+        context_file_path = mentat_dir_path / "context.json"
+
+        if len(args) > 1:
+            stream.send("Only one context file can be saved at a time", style="warning")
+            return
+
+        if len(args) == 1:
+            try:
+                context_file_path = Path(args[0]).expanduser().resolve()
+            except RuntimeError as e:
+                raise PathValidationError(
+                    f"Invalid context file path provided: {args[0]}: {e}"
+                )
+
+        if len(args) == 0:
+            stream.send(
+                "No context file specified. Defaulting to context.json", style="warning"
+            )
+
+        with open(context_file_path, "w") as file:
+            json.dump(code_context.include_files, file)
+
+        stream.send(f"Context saved to {context_file_path}", style="success")
+
+    @override
+    @classmethod
+    def arguments(cls) -> List[CommandArgument]:
+        return [CommandArgument("optional", ["path"])]
+
+    @override
+    @classmethod
+    def argument_autocompletions(
+        cls, arguments: list[str], argument_position: int
+    ) -> list[str]:
+        return get_command_filename_completions(arguments[-1])
+
+    @override
+    @classmethod
+    def help_message(cls) -> str:
+        return "Saves the current context to a file."
